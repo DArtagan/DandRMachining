94c47af60d282d6068d7124456ef566ae6fa47ee
William Weiskopf <worldwidewilly@comcast.net>
2012-02-20 16:06:26 -0700

    Beginning of the mockup

---
+++ mockup/demo.php
@@ -0,0 +1,33 @@
+<html>
+   <head>
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+      <script src="js/bootstrap.js"></script>
+   </head>
+   <body>
+      <div class="navbar navbar-fixed-top">
+         <div class="navbar-inner">
+            <div class="container">
+               <a href="#">Test 1</a>
+               <a href="#">Test 2</a>
+               <a href="#">Test 3</a>
+            </div>
+         </div>
+      </div>
+
+      <div class="container">
+         <h1>Hello World</h1>
+         <div class="span4">
+            <h2>First Heading</h2>
+            <p>The side section</p>
+         </div>
+         <div class="span8">
+            <h2>Second Heading</h2>
+            <p>The main section</p>
+         </div>
+         <hr>
+         <footer>
+            <p>&copy; William Weiskopf 2012</p>
+         </footer>
+      </div>
+   </body>
+</html>
