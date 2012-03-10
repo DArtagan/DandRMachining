1fe4295f3c783439c31690d3b4642188631e83ba
William H. Weiskopf, IV <worldwidewilly@comcast.net>
2012-03-09 23:42:40 -0700

    Working on new mockup

---
+++ mockup/index2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+  <link rel="stylesheet" type="text/css" href="css/main.css" />
+	<title></title>
+</head>
+<body>
+	<div class="banner 1">
+    <img src="img/banner.jpg" />
+  </div>
+</body>
+</html>
