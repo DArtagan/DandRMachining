0d64b78fd2d7e0ccc40ba6b3e5dbe8cf456cb66b
William Weiskopf <worldwidewilly@comcast.net>
2012-03-12 22:27:14 -0600

    New mainpage mockup

--- mockup/index2.php
+++ mockup/index2.php
@@ -1,14 +1,30 @@
 <!DOCTYPE HTML>
 <html lang="en-US">
 <head>
-	<meta charset="UTF-8">
+  <meta charset="UTF-8">
   <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
   <link rel="stylesheet" type="text/css" href="css/main.css" />
-	<title></title>
+  <title>D&R CNC Machining, Inc.</title>
 </head>
 <body>
-	<div class="banner 1">
+  <div class="banner 1">
     <img class="logo" src="img/logo.png" />
   </div>
+  <div class="categories">
+    <a style="background-image: url('img/equipment.png'); background-position: center center; background-repeat: no-repeat;"><h2>Equipment</h2></a>
+    <a style="background-image: url('img/quality.png'); background-position: center center; background-repeat: no-repeat;"><h2>Quality</h2></a>
+    <a style="background-image: url('img/industry.png'); background-position: center center; background-repeat: no-repeat;"><h2>Industries</h2></a>
+      <a style="background-image: url('img/aboutUs.png'); background-position: center center; background-repeat: no-repeat;"><h2>About Us</h2></a>
+    <div class="other">
+      <ul>
+        <li><a>Contact</a></li>
+        <li><a>Quotations</a></li>
+        <li><a>Cool Projects</a></li>
+      </ul>
+    </div>
+  </div>
+  <footer>
+    <p>Copyright D&R CNC Machining, Inc. 2012</p>
+  </footer>
 </body>
 </html>
