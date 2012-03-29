27e8c52bfded5bc6891146124e41ee00ca794fe4
William Weiskopf <worldwidewilly@comcast.net>
2012-03-29 08:51:37 -0600

    Starting work on the main wordpress site

--- wp-content/themes/dandr-roots/front-page.php
+++ wp-content/themes/dandr-roots/front-page.php
@@ -1,20 +1,19 @@
+<?php
+/* Template Name: Home */
+?>
+
 <?php get_header(); ?>
+  <div class="topBanner one">
+    <img src="<?php echo $stylesheetdir ?>img/logo.png" class="logo" />
+  </div>
   <?php roots_content_before(); ?>
     <div id="content" class="<?php echo CONTAINER_CLASSES; ?>">
-    <?php roots_main_before(); ?>
-      <div id="main" class="<?php echo MAIN_CLASSES; ?>" role="main">
-        <?php roots_loop_before(); ?>
-        <?php get_template_part('loop', 'page'); ?>
-        <?php roots_loop_after(); ?>
-      </div><!-- /#main -->
-    <?php roots_main_after(); ?>
-    <?php roots_sidebar_before(); ?>
-      <aside id="sidebar" class="<?php echo SIDEBAR_CLASSES; ?>" role="complementary">
-      <?php roots_sidebar_inside_before(); ?>
-        <?php get_sidebar(); ?>
-      <?php roots_sidebar_inside_after(); ?>
-      </aside><!-- /#sidebar -->
-    <?php roots_sidebar_after(); ?>
+      <?php roots_main_before(); ?>
+        <div id="main" class="<?php echo MAIN_CLASSES; ?>" role="main">
+        </div><!-- /#main -->
+      <?php roots_main_after(); ?>
+      <?php roots_sidebar_before(); ?>
+      <?php roots_sidebar_after(); ?>
     </div><!-- /#content -->
   <?php roots_content_after(); ?>
 <?php get_footer(); ?>
