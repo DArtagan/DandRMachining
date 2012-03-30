525f2ce0a57533f73b6c732aede02a6e0460a370
William Weiskopf <worldwidewilly@comcast.net>
2012-03-30 16:41:24 -0600

    Tweaking the inc/roots-config.php file to make the layout reactive (fluid).

---
+++ wp-content/themes/dandr-roots/inc/roots-config.php
@@ -0,0 +1,12 @@
+<?php
+
+define('POST_EXCERPT_LENGTH',  40);
+define('BOOTSTRAP_RESPONSIVE', true);
+define('WRAP_CLASSES',         'container-fluid');
+define('CONTAINER_CLASSES',    'row-fluid');
+define('MAIN_CLASSES',         'span8');
+define('SIDEBAR_CLASSES',      'span4');
+define('FULLWIDTH_CLASSES',    'span12');
+define('GOOGLE_ANALYTICS_ID',  '');
+
+?>
