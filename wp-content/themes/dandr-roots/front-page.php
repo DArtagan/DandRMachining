137890f3bdd3d6e4deb52c05a9a48b0e8bac06b6
William Weiskopf <worldwidewilly@comcast.net>
2012-04-19 23:44:44 -0600

    One more vertical spacing fix on the front-page

--- wp-content/themes/dandr-roots/front-page.php
+++ wp-content/themes/dandr-roots/front-page.php
@@ -6,8 +6,6 @@
   <?php roots_content_before(); ?>
     <div id="content" class="<?php echo CONTAINER_CLASSES; ?>">
       <?php roots_main_before(); ?>
-        <div id="main" class="<?php echo MAIN_CLASSES; ?>" role="main">
-        </div><!-- /#main -->
       <div class="categories container">
         <a class="span2" href="equipment" style="background-image: url('wp-content/themes/dandr-roots/img/equipment.png'); background-position: center center; background-repeat: no-repeat;"><h2>Equipment</h2></a>
         <a class="span2" href="child.php" style="background-image: url('wp-content/themes/dandr-roots/img/quality.png'); background-position: center center; background-repeat: no-repeat;"><h2>Quality</h2></a>
