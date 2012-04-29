a1a926f8eb13e9e29d81d7ab78da731c35625c47
William Weiskopf <worldwidewilly@comcast.net>
2012-04-28 22:40:02 -0600

    Trying to unload sidebar

--- wp-content/themes/dandr-roots/functions.php
+++ wp-content/themes/dandr-roots/functions.php
@@ -2,6 +2,9 @@
 /***************
 RUN after parent functions.php
 ***************/
+
+register_sidebar(array('name' => 'test', 'id' => 'test'));
+
 function dandr_post_parent_setup() {
 /*
  *  Register the custom menus for the theme
@@ -11,6 +14,19 @@ function dandr_post_parent_setup() {
       'footerNav' => 'Footer Navigation Menu'
     )
   );
+
+  
+/*
+ * Unregister sidebar for pages
+ */
+ unregister_sidebar( 'Sidebar' );
 }
 add_action( 'after_setup_theme', 'dandr_post_parent_setup', 11);
+
+function my_unregister_sidebars() {
+    unregister_sidebar( 'Sidebar' );
+            }
+
+            //unregister sidebars by tying onto same hook again, with priority '11'
+            add_action( 'widgets_init', 'my_unregister_sidebars', 11 );
 ?>
