4bf3c24087a6f5e48cd43d2d0415acb6b984045f
William Weiskopf <worldwidewilly@comcast.net>
2012-05-28 11:20:36 -0600

    Added address info to footer

--- wp-content/themes/dandr-roots/footer.php
+++ wp-content/themes/dandr-roots/footer.php
@@ -9,6 +9,16 @@
       <?php dynamic_sidebar('roots-footer'); ?>
       <p class="copy"><small>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></small></p>
     </div>
+    <div class="vcard">
+      <span class="fn org">D&amp;R CNC Machining, Inc.</span>
+      <a href="mailto:sales@dandrmachining.com" class="email">sales@dandrmachining.com</a>
+      <div class="adr">
+        <div class="street-address">791 Southpark Drive</div>
+        <div class="extended-address">Suite 900</div>
+        <span class="locality">Littleton</span>, <span class="region">CO</span>, <span class="postal-code">80120</span>
+      </div>
+      <div class="tel">303-781-2949</div>
+    </div>
   </footer>
   <?php roots_footer_after(); ?>
 
