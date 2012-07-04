0c256215aa9ad641311bbb435c5a641dabdec43a
William Weiskopf <worldwidewilly@comcast.net>
2012-07-03 21:35:00 -0600

    Moving header customizations into the functions file.

--- functions.php
+++ functions.php
@@ -1,5 +1,28 @@
 <?php 
 
+add_action("roots_header_before", "dandr_customHeader");
+
+function dandr_customHeader() {
+  echo '<div id="superWrap">
+    <header id="banner" class=" " role="banner">'
+      . roots_header_inside() . 
+      '<div id="topBanner" class="span12">
+        <a class="brand" href="' . home_url() . '" title="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" rel="home"><img class="logo" src="' . get_stylesheet_directory_uri() . '/img/logo.png" /></a>
+      </div>';
+      if(!is_front_page()) {
+        echo '<div class="navbar">
+          <div class="navbar-inner">
+            <div class="' . WRAP_CLASSES . '">
+              <nav id="nav-main" class="" role="navigation">'
+                . wp_nav_menu(array('theme_location' => 'primary_navigation', 'walker' => new Roots_Navbar_Nav_Walker())) . 
+              '</nav>
+            </div>
+          </div>
+        </div>';
+      }
+    echo '</header>';
+}
+
 /***************
 RUN after parent functions.php
 ***************/
